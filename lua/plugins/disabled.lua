101e8df50c8250524aa1e8cf7a49c10d9b6ed4a2
g1R87 <giri.sujan87@gmail.com>
2024-08-30 15:12:12 +0545

    update nvim lualine

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "neo-tree.nvim", enabled = false },
   { "neo-tree.nvim", enabled = false },
   { "sheerun/vim-polyglot", enabled = false },
+  -- { "echasnovski/mini.indentscope", enabled = false },
 }
