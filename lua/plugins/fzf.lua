9ba8c05dfedcae0cfec6715667d0f66db8a6af80
g1R87 <giri.sujan87@gmail.com>
2025-03-19 15:00:12 +0545

    add yazi

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,14 +1,12 @@
 return {
   "ibhagwan/fzf-lua",
   cmd = "FzfLua",
-  opts = function()
-    return {
-      winopts = {
-        width = 1,
-        preview = {
-          horizontal = "right:45%",
-        },
+  opts = {
+    winopts = {
+      width = 1,
+      preview = {
+        horizontal = "right:45%",
       },
-    }
-  end,
+    },
+  },
 }
